24a4b03ed1c60c6983d8d75bcdd0cc80e708e3a0
Mostafa-Elnagar <mostafaelnagar.ds@gmail.com>
2025-08-24 22:25:51 +0300

    fix registry issues

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -3,12 +3,10 @@
 def call(Map config = [:]) {
     def imageName = config.imageName ?: 'java-app'
     def imageTag = config.imageTag ?: 'latest'
-    def registryUrl = config.registryUrl ?: 'docker.io'
-    def fullImageName = "${registryUrl}/${imageName}:${imageTag}"
+    def fullImageName = "${imageName}:${imageTag}"
     
     echo "Pushing Docker image: ${fullImageName}"
     
-    sh "docker tag ${imageName}:${imageTag} ${fullImageName}"
     sh "docker push ${fullImageName}"
     
     echo "Docker image pushed successfully: ${fullImageName}"
